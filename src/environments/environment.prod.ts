5df3a00ac611d1ee4563cd9befaa0df9d120f225
unknown <rodolfhoazevedo@gmail.com>
2022-05-03 18:34:41 -0300

    [feature] add firebase auth, firestore and host to the project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'angular-calendar-3b93c',
+    appId: '1:1080678432908:web:11ff44e04531c266517ba9',
+    storageBucket: 'angular-calendar-3b93c.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCGXgXOlch0gFKohX_xLE56kGk2DbxLdc4',
+    authDomain: 'angular-calendar-3b93c.firebaseapp.com',
+    messagingSenderId: '1080678432908',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'angular-calendar-3b93c',
+    appId: '1:1080678432908:web:11ff44e04531c266517ba9',
+    storageBucket: 'angular-calendar-3b93c.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCGXgXOlch0gFKohX_xLE56kGk2DbxLdc4',
+    authDomain: 'angular-calendar-3b93c.firebaseapp.com',
+    messagingSenderId: '1080678432908',
+  },
   production: false
 };
 
