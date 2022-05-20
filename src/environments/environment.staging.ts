07b909e9e0838bc247fde256a38b44514600aeb6
unknown <rodolfhoazevedo@gmail.com>
2022-05-19 22:58:50 -0300

    [fix] fix env for test project

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyDtCWTa9lOpn56t33pYP_G0EosVyag2vLo",
+    authDomain: "angular-calendar-test.firebaseapp.com",
+    projectId: "angular-calendar-test",
+    storageBucket: "angular-calendar-test.appspot.com",
+    messagingSenderId: "154491307965",
+    appId: "1:154491307965:web:6a5515504f8225dc2534d4"
+  },
+  production: true
+};
